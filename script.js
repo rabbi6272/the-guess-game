7a609418406b23ee9476aa317bb4ce64d657ae4d
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2023-10-14 21:54:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const ans = document.getElementById('realAns');
+const result = document.getElementById('result-box');
+const score = document.getElementById('score');
+const button = document.querySelector('button');
+const chances = document.getElementById('chances');
+
+
+
+
+
+    
+num = Math.trunc(Math.random() * 10 +1);
+    let s = 0;
+    let c = 2;
+button.addEventListener('click', function () {
+    if(c!=-1){
+        var inputNum = Number(document.querySelector('input').value);
+        if(!inputNum){
+        result.innerHTML = "Please enter a number"
+        } else{
+            chances.innerHTML=c--;
+            if (inputNum == num) {
+                s++;
+                score.textContent=s;
+                result.innerHTML= "You guessed correctly";
+            } else if (inputNum > num) {
+                result.innerHTML="Too high"
+            } else if (inputNum < num) {
+                result.innerHTML="Too low" 
+            } 
+        }
+    } else{return ans.innerHTML=num;}
+})
